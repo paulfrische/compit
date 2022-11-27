03193e1ef2f78ea61299f27c9c40b50cdd864d3e
Paul Frische <paul@paulfrische.xyz>
2022-11-27 11:35:47 +0100

    prompt can be disabled

--- lua/compit/init.lua
+++ lua/compit/init.lua
@@ -35,7 +35,7 @@ local function set_command(command)
     io.close(file_obj)
 end
 
-M.run = function()
+local function run_with_prompt()
     local command = get_command()
     vim.ui.input({ prompt = "Build Command: ", default = command }, function(input)
         if input == nil then
@@ -48,6 +48,18 @@ M.run = function()
         vim.cmd('vsplit | terminal ' .. command)
         vim.cmd('startinsert')
     end)
+end
+
+M.run = function(options)
+    if options.prompt ~= nil then
+        if options.prompt then
+            local command = get_command()
+            vim.cmd('vsplit | terminal ' .. command)
+            vim.cmd('startinsert')
+        end
+    else
+        run_with_prompt()
+    end
 end
 
 return M
